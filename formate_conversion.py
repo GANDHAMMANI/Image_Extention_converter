e1898cb6fb774e79909033693fcfc96a5285fb14
Saketh07 <107030215+GANDHAMMANI@users.noreply.github.com>
2024-05-20 02:34:57 +0530

    Update formate_conversion.py

--- formate_conversion.py
+++ formate_conversion.py
@@ -45,32 +45,36 @@ if uploaded_file is not None:
             # Convert the image
             converted_image = io.BytesIO()
             
-            # Handle PDF conversion
-            if format_to_convert == "PDF":
-                # Save image to temporary file
-                with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_file:
-                    temp_file.write(image_data)
-                    temp_file_path = temp_file.name
-
-                c = canvas.Canvas(converted_image)
-                c.drawImage(temp_file_path, 0, 0)
-                c.save()
-                mime = "application/pdf"
+            # Handle unsupported SVG conversion
+            if format_to_convert == "SVG":
+                st.error("Conversion to SVG is not supported by PIL. Please choose another format.")
             else:
-                # Set high quality option
-                quality = DEFAULT_QUALITY
-                image.save(converted_image, format=format_to_convert, quality=quality)
-                mime = f"image/{format_to_convert.lower()}"
-            
-            converted_image.seek(0)
-            
-            # Provide download link
-            st.download_button(
-                label="Download Converted Image",
-                data=converted_image,
-                file_name=f"converted_image.{format_to_convert.lower()}",
-                mime=mime
-            )
+                # Handle PDF conversion
+                if format_to_convert == "PDF":
+                    # Save image to temporary file
+                    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_file:
+                        temp_file.write(image_data)
+                        temp_file_path = temp_file.name
+
+                    c = canvas.Canvas(converted_image)
+                    c.drawImage(temp_file_path, 0, 0)
+                    c.save()
+                    mime = "application/pdf"
+                else:
+                    # Set high quality option
+                    quality = DEFAULT_QUALITY
+                    image.save(converted_image, format=format_to_convert, quality=quality)
+                    mime = f"image/{format_to_convert.lower()}"
+                
+                converted_image.seek(0)
+                
+                # Provide download link
+                st.download_button(
+                    label="Download Converted Image",
+                    data=converted_image,
+                    file_name=f"converted_image.{format_to_convert.lower()}",
+                    mime=mime
+                )
     except UnidentifiedImageError:
         st.error("The uploaded file is not a valid image or is not supported. Please upload a valid image file.")
     except Exception as e:
